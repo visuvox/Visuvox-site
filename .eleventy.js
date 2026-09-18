2823f01e194c853ba879202f542599b027164f3f
visuvox <francoiswathdielmy@gmail.com>
2026-09-18 13:13:21 -0400

    Initialize Eleventy configuration
    Configure Eleventy with passthrough copies and collections for posts and gallery.

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/*.md").sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addCollection("gallery", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/gallery/*.md");
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
