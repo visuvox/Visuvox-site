c50f8322797c274fd1a4f7c3fb6a696a58d2f934
visuvox <francoiswathdielmy@gmail.com>
2026-09-18 17:39:05 -0400

    Add date filter using Luxon in Eleventy config
    Added a date filter using Luxon for formatting dates.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+
+  eleventyConfig.addFilter("date", (dateObj, format) => {
+    return DateTime.fromJSDate(new Date(dateObj), { zone: "utc" }).toFormat(format || "dd MMMM yyyy");
+  });
 
   eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/blog/*.md").sort((a, b) => b.date - a.date);
